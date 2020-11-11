32a5eff506b912bba2149ff0e069ee858c9ff118
guscappe <grcappelletti@gmail.com>
2020-11-11 14:02:08 -0300

    ya tenia todo hecho, solo agregue el campo tipo de modificacion

---
+++ DDL/02 cc-1131 Entidades.sql
@@ -0,0 +1,2 @@
+ALTER TABLE aud_modif_horas_trabajadas 
+	ADD tipo_accion CHAR(1);
--- desarrollo/03 trigger-modificacion-liq-mensual.sql
+++ desarrollo/03 trigger-modificacion-liq-mensual.sql
@@ -10,11 +10,11 @@ CREATE TRIGGER trg_actualiza_horas_trabajadas
 		
 		DECLARE diferencia INT DEFAULT 0;
 		declare nue_valor INT DEFAULT 0;
-		# si el resultado da negatico hay que quitar horas en la liq_mensual
+		
 		set diferencia = OLD.horas - NEW.horas;
 		
-		INSERT INTO aud_modif_horas_trabajadas (horas_old, horas_new, mes, usuario , id_rol , project, dia, dif)
-		VALUES (OLD.horas,NEW.horas,OLD.mes,CURRENT_USER(),OLD.id_rol,OLD.id_project, NOW(), diferencia);
+		INSERT INTO aud_modif_horas_trabajadas (horas_old, horas_new, mes, usuario , id_rol , project, dia, dif,tipo_accion)
+		VALUES (OLD.horas,NEW.horas,OLD.mes,CURRENT_USER(),OLD.id_rol,OLD.id_project, NOW(), diferencia.'U');
 
 		IF ( select COUNT(id_project) FROM liq_mensual WHERE  id_project = OLD.id_project and mes= OLD.mes GROUP BY id_project ) THEN 
 
@@ -33,3 +33,7 @@ CREATE TRIGGER trg_actualiza_horas_trabajadas
 
 	END;
 $$
+
+
+
+
